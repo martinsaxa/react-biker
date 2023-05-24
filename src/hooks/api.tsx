7a73c44de95fbab60cd706a04f277a14e717d86a
Martin Saxa <martinsaxa0@gmail.com>
2023-05-24 20:01:13 +0200

    do not refetch on network screen navigation

--- src/hooks/api.tsx
+++ src/hooks/api.tsx
@@ -1,6 +1,18 @@
-import useSWR from 'swr'
+import useSWRImmutable from 'swr/immutable'
 import { NetworksRequest } from '../types/api'
 
 export const useNetworks = () => {
-    return useSWR<NetworksRequest>('/v2/networks')
+    return useSWRImmutable<NetworksRequest>('/v2/networks')
+}
+
+export const useNetwork = (id: string) => {
+    const { data, ...rest } = useNetworks()
+    if (data) {
+        const network = data.networks.find((item) => item.id === id)
+        return {
+            data: network,
+            ...rest
+        }
+    }
+    return {data, ...rest}
 }
--- src/screens/NetworkScreen.tsx
+++ src/screens/NetworkScreen.tsx
@@ -1,11 +1,14 @@
 import { View } from "react-native";
 import { Text } from "react-native-paper";
 import { NetworkProps } from ".";
+import { useNetwork } from "../hooks/api";
 
 export default function NetworkScreen({ route }: NetworkProps) {
+    const { data } = useNetwork(route.params.id)
+
     return (
         <View style={{ flex: 1 }}>
-            <Text>{route.params.id}</Text>
+            <Text>{JSON.stringify(data)}</Text>
         </View>
     )
 }
