067539b555bc0b425373c34ecc083c1dd708e14b
Martin Saxa <martinsaxa0@gmail.com>
2023-05-24 15:57:04 +0200

    install: react native paper

--- App.tsx
+++ App.tsx
@@ -1,17 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { Stack } from './src/screens';
 import HomeScreen from './src/screens/HomeScreen';
 import LocationDetailsScreen from './src/screens/LocationDetailsScreen';
+import { PaperProvider, useTheme } from 'react-native-paper';
+import { CombinedDefaultTheme } from './src/lib/theme';
 
 export default function App() {
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={HomeScreen} />
-        <Stack.Screen name='LocationDetails' component={LocationDetailsScreen} />
-      </Stack.Navigator>
+    <NavigationContainer theme={CombinedDefaultTheme}>
+      <PaperProvider theme={CombinedDefaultTheme}>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeScreen} />
+          <Stack.Screen name='LocationDetails' component={LocationDetailsScreen} />
+        </Stack.Navigator>
+      </PaperProvider>
     </NavigationContainer>
   );
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,12 @@
-import { Button, Text, View } from "react-native"
+import { View } from "react-native"
+import { Button, Text } from "react-native-paper"
 import { HomeProps } from "."
 
 export default function HomeScreen({ navigation }: HomeProps) {
     return (
         <View style={{ flex: 1 }}>
             <Text>Home</Text>
-            <Button onPress={() => navigation.navigate('LocationDetails')} title="Press" />
+            <Button onPress={() => navigation.navigate('LocationDetails')} mode="contained">Click</Button>
         </View>
     )
 }
